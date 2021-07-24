09c27861627981e2716966e8139fdf281b6d0201
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-25 01:03:15 +0200

    start monthly_challenges project

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def january(response):
+    return HttpResponse("This is January")
+
+
+def february(response):
+    return HttpResponse("This is February")
+
+
+def march(response):
+    return HttpResponse("This is March").
+
+
