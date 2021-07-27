b9f5614d6464640eee3268e36e432d1f8ed8b2d6
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-28 01:56:19 +0200

    21_Adding More Dynamic View Logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,6 +2,23 @@ from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+# dictionary of months and its advices
+monthly_challenges = {
+    "january":  "january",  
+    "february":  "february",  
+    "march":  "march",  
+    "april":  "april",  
+    "may":  "may",  
+    "june":  "june",  
+    "july":  "july",  
+    "august":  "august",  
+    "september":  "september",  
+    "october":  "october",  
+    "november":  "november",  
+    "december":  "december",  
+}
+
 # Create your views here.
 def monthly_challenge_by_number(request, month):
     if month>=1 and month<=12:
@@ -11,14 +28,9 @@ def monthly_challenge_by_number(request, month):
     
 
 def monthly_challenge(response, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "January"
-    elif month == "february":
-        challenge_text = "February"
-    elif month == "march":
-        challenge_text = "March"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This Month is Not Supported")
-
-    return HttpResponse(challenge_text)
+    
