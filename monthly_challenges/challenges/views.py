ce6fe3c214c8dfe00f96d4c263bd3568b5a027c3
EbrahimStd <ibrahim267745@eng.zu.edu.eg>
2021-07-28 02:23:43 +0200

    22_Redirects

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,31 +1,35 @@
-from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 
 # dictionary of months and its advices
 monthly_challenges = {
-    "january":  "january",  
-    "february":  "february",  
-    "march":  "march",  
-    "april":  "april",  
-    "may":  "may",  
-    "june":  "june",  
-    "july":  "july",  
-    "august":  "august",  
-    "september":  "september",  
-    "october":  "october",  
-    "november":  "november",  
-    "december":  "december",  
+    "january":  "january",
+    "february":  "february",
+    "march":  "march",
+    "april":  "april",
+    "may":  "may",
+    "june":  "june",
+    "july":  "july",
+    "august":  "august",
+    "september":  "september",
+    "october":  "october",
+    "november":  "november",
+    "december":  "december",
 }
 
 # Create your views here.
+
+
 def monthly_challenge_by_number(request, month):
-    if month>=1 and month<=12:
-        return HttpResponse(month)
-    else:
-        return HttpResponseNotFound("Month Number is Not Supported")
-    
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month number")
+
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 
 def monthly_challenge(response, month):
     try:
@@ -33,4 +37,3 @@ def monthly_challenge(response, month):
         return HttpResponse(challenge_text)
     except:
         return HttpResponseNotFound("This Month is Not Supported")
-    
